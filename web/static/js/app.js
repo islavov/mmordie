77399fb0951ce5a8aac52643947b0b897b1c9d34
Ivelin Slavov <ivelin@uber.com>
2016-01-29 22:38:00 +0200

    Basic phaser bootstrap

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -2,10 +2,14 @@
 
 import GameState from './states/GameState';
 
+var w = window.innerWidth * window.devicePixelRatio;
+var h = window.innerHeight * window.devicePixelRatio;
+
 class Game extends Phaser.Game {
 
 	constructor() {
-		super(500, 500, Phaser.AUTO, 'content', null);
+		super((h > w) ? h:w, (h > w) ? w:h, Phaser.CANVAS, 'content', null);
+		//super(800, 800, Phaser.CANVAS, 'content', null);
 		this.state.add('GameState', GameState, false);
 		this.state.start('GameState');
 	}
