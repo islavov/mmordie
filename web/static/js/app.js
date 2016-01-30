2f893ce4e78234918e14b9fd1f77828204b7a196
Ivelin Slavov <ivelin@uber.com>
2016-01-30 15:20:21 +0200

    Minor changes

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -4,20 +4,35 @@ import GameState from './states/GameState';
 import Sync from './socket';
 import uuid from 'node-uuid';
 
-var w = window.innerWidth * window.devicePixelRatio;
-var h = window.innerHeight * window.devicePixelRatio;
 
-class Game extends Phaser.Game {
+function initGame(){
+	var w = window.innerWidth * window.devicePixelRatio;
+	var h = window.innerHeight * window.devicePixelRatio;
 
-	constructor() {
-		super((h > w) ? h:w, (h > w) ? w:h, Phaser.CANVAS, 'content', null);
-		//super(800, 800, Phaser.CANVAS, 'content', null);
-		this.userID = uuid.v4();
-		this.sync = new Sync(this.userID);
-		this.state.add('GameState', GameState, false);
-		this.state.start('GameState');
+	var game = new Phaser.Game(
+		(h > w) ? h : w,
+		(h > w) ? w : h,
+		Phaser.CANVAS,
+		'content',
+		{
+			create: onCreate,
+			preload: onPreload
+		}
+	);
+
+	function onPreload(){
+			game.load.image("hexagon", "images/hexagon.png");
+	}
+
+	function onCreate() {
+		game.userID = uuid.v4();
+		game.sync = new Sync(this.userID);
+		this.game.world.setBounds(0, 0, 2000, 2000);
+
+		game.state.add('GameState', GameState, false);
+		game.state.start('GameState');
 	}
 
 }
-new Game();
 
+initGame();
--- web/static/js/states/GameState.js
+++ web/static/js/states/GameState.js
@@ -3,8 +3,8 @@ import Enemy from '../objects/Enemy';
 
 class GameState extends Phaser.State {
 
-	create() {
-		var ufo = [
+  create() {
+    var ufo = [
       '....DDDDDDDD....',
       '...DDEEDDDDDD...',
       '..DDDEEDDDDDDD..',
@@ -22,66 +22,68 @@ class GameState extends Phaser.State {
       '.66....66....66.'
     ];
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
-    this.game.stage.backgroundColor = '#124184';
-    this.game.create.texture('alien', ufo, 6, 6);
+    this.game.create.texture('alien', ufo, 4, 4);
 
+    //this.map = this.game.add.tilemap();
 
-		this.game.world.setBounds(0, 0, 2000, 2000);
-    let center = { x: this.game.world.centerX, y: this.game.world.centerY };
+    //this.map.addTilesetImage('hexagon', 'hexagon');
+
+    //this.layer = this.map.createLayer('Ground');
+    //this.layer.resizeWorld();
+
+    let center = {x: this.game.world.centerX, y: this.game.world.centerY};
 
     this.others = {};
     this.enemies = this.game.add.physicsGroup();
-		this.player = new Player(this.game, center.x, center.y, 'alien');
+    this.player = new Player(this.game, center.x, center.y, 'alien');
     this.game.camera.follow(this.player, Phaser.Camera.FOLLOW_PLATFORMER);
 
-		this.cursors = this.game.input.keyboard.createCursorKeys();
-	  this.game.sync.chan.on('new:player_position', this.syncPositions.bind(this))
+    this.cursors = this.game.input.keyboard.createCursorKeys();
+    this.game.sync.chan.on('new:player_position', this.syncPositions.bind(this))
   }
 
-	update() {
+  update() {
 
-		this.game.sync.syncPlayer(this.player);
+    this.game.sync.syncPlayer(this.player);
     var speed = 400;
 
-    if (this.game.physics.arcade.collide(this.player, this.enemies)){
+    if (this.game.physics.arcade.collide(this.player, this.enemies)) {
       this.player.body.bounce.setTo(1, 1);
       this.player.inactive = true;
-      this.game.time.events.add(Phaser.Timer.SECOND / 5, function(){this.player.inactive = false}, this);
+      this.game.time.events.add(Phaser.Timer.SECOND / 5, function () {
+        this.player.inactive = false
+      }, this);
     }
 
-    if (this.player.inactive){
+    if (this.player.inactive) {
       return
     }
 
     this.player.body.velocity.x = 0;
     this.player.body.velocity.y = 0;
 
-    if (this.cursors.up.isDown)
-    {
-        this.player.body.velocity.y = -speed
+    if (this.cursors.up.isDown) {
+      this.player.body.velocity.y = -speed
     }
-    else if (this.cursors.down.isDown)
-    {
-        this.player.body.velocity.y = speed
+    else if (this.cursors.down.isDown) {
+      this.player.body.velocity.y = speed
     }
 
-    if (this.cursors.left.isDown)
-    {
-        this.player.body.velocity.x = -speed
+    if (this.cursors.left.isDown) {
+      this.player.body.velocity.x = -speed
     }
-    else if (this.cursors.right.isDown)
-    {
-        this.player.body.velocity.x = speed
+    else if (this.cursors.right.isDown) {
+      this.player.body.velocity.x = speed
 
     }
 
   }
 
-  syncPositions(playerData){
+  syncPositions(playerData) {
     if (playerData.user == this.game.userID) {
       return;
     }
-    if (typeof this.others[playerData.user] === 'undefined'){
+    if (typeof this.others[playerData.user] === 'undefined') {
       var other_player = new Enemy(this.game,
         playerData.position.x, playerData.position.y, 'alien', playerData.options.tint);
       this.others[playerData.user] = other_player;
@@ -98,12 +100,12 @@ class GameState extends Phaser.State {
 
   }
 
-	render() {
+  render() {
 
     this.game.debug.cameraInfo(this.game.camera, 32, 32);
     this.game.debug.spriteCoords(this.player, 32, 500);
 
-}
+  }
 
 }
 
