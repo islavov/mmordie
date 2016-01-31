35a0f95663425af4ac421e458c2078ddf7206e70
Kirichev, Alexander <alexander.kirichev@sap.com>
2016-01-31 02:06:14 +0200

    Smooth scrolling

--- landing/assets/js/jquery.custom.js
+++ landing/assets/js/jquery.custom.js
@@ -21,3 +21,18 @@ if(img.length > 0){
     }
     $(document).mousemove(mouse);
 }
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
